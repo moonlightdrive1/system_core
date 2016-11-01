6e55315821b3c7a6529642594352faf445782827
Aaron Nixon <aarondoom@cookiedoom.com>
2016-11-01 15:48:19 -0700

    Less CM
    Change-Id: I832da26bdbb0f09383c88d8bbd23c26d9a0e22d7

--- debuggerd/tombstone.cpp
+++ debuggerd/tombstone.cpp
@@ -167,13 +167,13 @@ static const char* get_sigcode(int signo, int code) {
 static void dump_header_info(log_t* log) {
   char fingerprint[PROPERTY_VALUE_MAX];
   char revision[PROPERTY_VALUE_MAX];
-  char cm_version[PROPERTY_VALUE_MAX];
+  char aoscp_version[PROPERTY_VALUE_MAX];
 
-  property_get("ro.cm.version", cm_version, "unknown");
+  property_get("ro.aoscp.version", aoscp_version, "unknown");
   property_get("ro.build.fingerprint", fingerprint, "unknown");
   property_get("ro.revision", revision, "unknown");
 
-  _LOG(log, logtype::HEADER, "CM Version: '%s'\n", cm_version);
+  _LOG(log, logtype::HEADER, "AOSCP Version: '%s'\n", aoscp_version);
   _LOG(log, logtype::HEADER, "Build fingerprint: '%s'\n", fingerprint);
   _LOG(log, logtype::HEADER, "Revision: '%s'\n", revision);
   _LOG(log, logtype::HEADER, "ABI: '%s'\n", ABI_STRING);
